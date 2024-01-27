e24b93f0a137f95848c87b9970219cd450677e6f
celularfreak <celularfreak@hotmail.com>
2024-01-27 12:50:27 +0100

    Task3 Done

---
+++ src/Task3.java
@@ -0,0 +1,14 @@
+
+public class Task3 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        int term1 = x * x;
+        int term2 = ((y * 4) / 5 - x);
+        int term3 = term2 * term2;
+
+        int result = term1 + term3;
+        System.out.println(result);
+    }
+}
