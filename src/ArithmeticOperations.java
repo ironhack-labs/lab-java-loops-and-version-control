7b306bddc49cb705514a8bd0bf382380785180c8
evagapi <87602505+evagapi@users.noreply.github.com>
2024-02-02 17:36:57 +0100

    refactor: consolidate all the methods in a single class and apply suggestions from the feedback

--- src/ArithmeticOperations.java
+++
@@ -1,8 +0,0 @@
-public class ArithmeticOperations {
-    public static double magicFormula(double x, double y) {
-        double leftSide = Math.pow(x, 2);
-        double rightSide = Math.pow(((4 * y) / 5) - x, 2);
-
-        return  leftSide + rightSide;
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers1 = {1, 3, 5, 7, 9};
         int[] numbers2 = {20, 7, 2, 10, 1, 8,};
-        double result = ArithmeticOperations.magicFormula(2d, 3d);
 
-        System.out.println(DiffMaxMin.calculateDifference(numbers1));
-        System.out.println(smallestAndSecondSmallest.findSmallestAndSecondSmallest(numbers2));
-        System.out.println(result);
+        System.out.println(MathGenie.calculateDifference(numbers1));
+        System.out.println(Arrays.toString(MathGenie.findSmallestAndSecondSmallest(numbers2)));
+        System.out.println(MathGenie.magicFormula(2d, 3d));
     }
 }
